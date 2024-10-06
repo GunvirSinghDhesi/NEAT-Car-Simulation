008e36f709c61531bf9855c1f66f5ae90ca70685
Gunvir Dhesi <gdhesi2005@gmail.com>
2024-10-05 23:12:49 -0400

    final cchanges

--- delete.py
+++ delete.py
@@ -1,23 +1,22 @@
 import pygame
-import neat
 import os
-import sys
 import math
+import sys
+import neat
 
-# Setting up screen for the simulation
-screen_width = 800
-screen_height = 556
-SCREEN = pygame.display.set_mode((screen_width, screen_height))
+SCREEN_WIDTH = 1244
+SCREEN_HEIGHT = 1016
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-TRACK = pygame.image.load(os.path.join("Assets", "track1.png"))
+TRACK = pygame.image.load(os.path.join("Assets", "track.png"))
 
-# Setting up car behavior
+
 class Car(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.original_image = pygame.image.load(os.path.join("Assets", "car.png"))
         self.image = self.original_image
-        self.rect = self.image.get_rect(center=(35, 50))
+        self.rect = self.image.get_rect(center=(490, 820))
         self.vel_vector = pygame.math.Vector2(0.8, 0)
         self.angle = 0
         self.rotation_vel = 5
@@ -29,10 +28,8 @@ class Car(pygame.sprite.Sprite):
         self.radars.clear()
         self.drive()
         self.rotate()
-
-        for radar_angle in (-90, -45, 0, 45, 90):
+        for radar_angle in (-60, -30, 0, 30, 60):
             self.radar(radar_angle)
-
         self.collision()
         self.data()
 
@@ -42,23 +39,18 @@ class Car(pygame.sprite.Sprite):
     def collision(self):
         length = 40
         collision_point_right = [int(self.rect.center[0] + math.cos(math.radians(self.angle + 18)) * length),
-                                int(self.rect.center[1] - math.sin(math.radians(self.angle + 18)) * length)]
+                                 int(self.rect.center[1] - math.sin(math.radians(self.angle + 18)) * length)]
         collision_point_left = [int(self.rect.center[0] + math.cos(math.radians(self.angle - 18)) * length),
                                 int(self.rect.center[1] - math.sin(math.radians(self.angle - 18)) * length)]
 
-        # Ensure points are within screen boundaries
-        if 0 <= collision_point_right[0] < screen_width and 0 <= collision_point_right[1] < screen_height and \
-        0 <= collision_point_left[0] < screen_width and 0 <= collision_point_left[1] < screen_height:
-            
-            # Die on Collision
-            if SCREEN.get_at(collision_point_right) == pygame.Color(2, 105, 31, 255) \
-                    or SCREEN.get_at(collision_point_left) == pygame.Color(2, 105, 31, 255):
-                self.alive = False
+        # Die on Collision
+        if SCREEN.get_at(collision_point_right) == pygame.Color(2, 105, 31, 255) \
+                or SCREEN.get_at(collision_point_left) == pygame.Color(2, 105, 31, 255):
+            self.alive = False
 
-            # Draw Collision Points
-            pygame.draw.circle(SCREEN, (0, 255, 255, 0), collision_point_right, 4)
-            pygame.draw.circle(SCREEN, (0, 255, 255, 0), collision_point_left, 4)
-
+        # Draw Collision Points
+        pygame.draw.circle(SCREEN, (0, 255, 255, 0), collision_point_right, 4)
+        pygame.draw.circle(SCREEN, (0, 255, 255, 0), collision_point_left, 4)
 
     def rotate(self):
         if self.direction == 1:
@@ -76,24 +68,17 @@ class Car(pygame.sprite.Sprite):
         x = int(self.rect.center[0])
         y = int(self.rect.center[1])
 
-        while length < 100:
+        while not SCREEN.get_at((x, y)) == pygame.Color(2, 105, 31, 255) and length < 200:
             length += 1
             x = int(self.rect.center[0] + math.cos(math.radians(self.angle + radar_angle)) * length)
             y = int(self.rect.center[1] - math.sin(math.radians(self.angle + radar_angle)) * length)
-
-            # Check if the x, y are within the screen boundaries
-            if x < 0 or x >= screen_width or y < 0 or y >= screen_height:
-                break
-
-            if SCREEN.get_at((x, y)) == pygame.Color(2, 105, 31, 255):
-                break
 
         # Draw Radar
         pygame.draw.line(SCREEN, (255, 255, 255, 255), self.rect.center, (x, y), 1)
         pygame.draw.circle(SCREEN, (0, 255, 0, 0), (x, y), 3)
 
         dist = int(math.sqrt(math.pow(self.rect.center[0] - x, 2)
-                            + math.pow(self.rect.center[1] - y, 2)))
+                             + math.pow(self.rect.center[1] - y, 2)))
 
         self.radars.append([radar_angle, dist])
 
@@ -103,10 +88,12 @@ class Car(pygame.sprite.Sprite):
             input[i] = int(radar[1])
         return input
 
+
 def remove(index):
     cars.pop(index)
     ge.pop(index)
     nets.pop(index)
+
 
 def eval_genomes(genomes, config):
     global cars, ge, nets
@@ -154,6 +141,7 @@ def eval_genomes(genomes, config):
             car.update()
         pygame.display.update()
 
+
 # Setup NEAT Neural Network
 def run(config_path):
     global pop
@@ -172,6 +160,7 @@ def run(config_path):
     pop.add_reporter(stats)
 
     pop.run(eval_genomes, 50)
+
 
 if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
